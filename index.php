4139c9a3c7d5398e5451c17d01f32394b8bec73f
Simone <506356@edu.rocmn.nl>
2017-05-22 13:14:38 +0200

    updated

--- index.php
+++ index.php
@@ -29,7 +29,7 @@ include('includes/header.php');
 		{
 			?>
             <div class="col-m-3">
-            <form method="post" action="winkelmandje.php?action=add&id=<?php echo $row["id"]; ?>">
+            <form method="post" action="index.php?action=add&id=<?php echo $row["id"]; ?>">
             <div align="center">
             <img src="<?php echo $row["image"]; ?>">
             <h5 class="text-info"><?php echo $row["p_name"]; ?></h5>
