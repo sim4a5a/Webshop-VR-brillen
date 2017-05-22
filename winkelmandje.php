c6204a4124730e615cbb600050be7466d0bdd1ac
Larstersteeg <460346@edu.rocmn.nl>
2017-05-22 10:44:08 +0200

    goede nummers

--- winkelmandje.php
+++ winkelmandje.php
@@ -25,7 +25,7 @@ foreach($_SESSION["cart"] as $keys => $values)
         <td><?php echo $values["item_name"]; ?></td>
         <td><input type="text" name="quantity" <?php echo $values["item_quantity"]?></td>
         <td>$ <?php echo $values["product_price"]; ?></td>
-        <td>$ <?php echo number_format($values["item_quantity"] * $values["product_price"], 5); ?></td>
+        <td>$ <?php echo number_format($values["item_quantity"] * $values["product_price"], 2); ?></td>
         <td><a href="winkelmandje.php?action=delete&id=<?php echo $values["product_id"]; ?>"><span class="text-danger">X</span></a></td>
         <td><a href="winkelmandje.php?action=update&id=<?php echo $values["item_quantity"]; ?>"><span class="text-danger">+</span></a></td>
         </tr>
